d16b21ffa3a67c213944d1aa3853018920b989db
Koen Schmeets <k.schmeets@gmail.com>
2018-10-19 01:31:41 +0200

    Add live reloading for ExtendScript code

--- src/js/extendscript.tsx
+++ src/js/extendscript.tsx
@@ -1,34 +1,14 @@
-import {
-  inCEPEnvironment,
-  loadExtendscript,
-  getExtensionPath,
-  getHostEnvironment,
-  RGBColor
-} from "cep-interface";
+import { evalExtendscript, getHostEnvironment, RGBColor } from 'cep-interface'
+import { readFileSync } from 'fs'
 
-if (inCEPEnvironment()) {
-  // @ts-ignore
-  const os = window.cep_node.require("os");
-  // @ts-ignore
-  const fs = window.cep_node.require("fs-extra");
-  // @ts-ignore
-  const path = window.cep_node.require("path");
-  // @ts-ignore
-  const { logger } = require("./logger");
+// @ts-ignore
+const Buffer = cep_node.Buffer
+const jsxBundle = readFileSync('./dist/index.js').toString()
+evalExtendscript(jsxBundle)
 
-  const extensionPath = getExtensionPath();
-
-  logger.info("start", extensionPath);
-
-  const manifest = fs.readJsonSync(path.join(extensionPath, "manifest.json"));
-  loadExtendscript(manifest["index.jsx.ts"]);
-
-  const host = getHostEnvironment();
-  if (host) {
-    const skin = host.appSkinInfo;
-    const bgColor = skin.panelBackgroundColor.color as RGBColor;
-    document.body.style.background = `rgb(${parseInt(bgColor.red)}, ${parseInt(bgColor.green)}, ${
-      parseInt(bgColor.blue)
-    })`;
-  }
+const host = getHostEnvironment();
+if (host) {
+  const skin = host.appSkinInfo;
+  const bgColor = skin.panelBackgroundColor.color as RGBColor;
+  document.body.style.backgroundColor = `rgba(${Math.round(bgColor.red)},${Math.round(bgColor.green)},${Math.round(bgColor.blue)},${bgColor.alpha})`
 }
