3651e0cc116e6cbf9e0d5beb983ae6f3304fd06f
Koen Schmeets <k.schmeets@gmail.com>
2018-10-19 17:25:04 +0200

    Refactor AdobeAppInfo

--- src/js/components/AdobeAppInfo.tsx
+++ src/js/components/AdobeAppInfo.tsx
@@ -2,7 +2,6 @@ import {
   evalExtendscript,
   getApplicationID,
   getExtensionPath,
-  inCEPEnvironment,
   openURLInDefaultBrowser,
 } from 'cep-interface'
 import * as React from 'react'
@@ -13,38 +12,16 @@ const debugPorts = JSON.parse(process.env.DEBUG_PORTS)
 const debugPort = debugPorts[getApplicationID()]
 
 export default class AdobeAppInfo extends React.Component {
-  state = {
-    id: undefined,
-    version: undefined,
-    name: undefined,
-    extensionPath: undefined,
-    isInCEPEnvironment: false
-  }
-
-  async componentDidMount() {
-    if (inCEPEnvironment()) {
-      const info: any = await evalExtendscript(`$.global["${id}"].getInfo()`)
-      const extensionPath = await getExtensionPath();
-      this.setState({
-        id: info.id,
-        name: info.name,
-        version: info.version,
-        extensionPath,
-        isInCEPEnvironment: true
-      })
-    }
-  }
-
-  render() {
+  async render() {
+    const info = await evalExtendscript(`$.global["${id}"].getInfo()`)
     return (
       <div className="AdobeAppInfo">
         <h3>Adobe App Info</h3>
-        {!this.state.isInCEPEnvironment && <p>Not in CEP environment.</p>}
         <ul>
-          <li>Id: {this.state.id}</li>
-          <li>Name: {this.state.name}</li>
-          <li>Version: {this.state.version}</li>
-          <li>Extension Path: {this.state.extensionPath}</li>
+          <li>Id: {info.id}</li>
+          <li>Name: {info.name}</li>
+          <li>Version: {info.version}</li>
+          <li>Extension Path: {await getExtensionPath()}</li>
         </ul>
         <button onClick={() => openURLInDefaultBrowser(`http://localhost:${debugPort}`)}>Open debugger</button>
       </div>
