0f12dc47bd08142cf2bda580295527841a676989
Koen Schmeets <k.schmeets@gmail.com>
2018-10-23 00:22:46 +0200

    Fix build order, AdobeAppInfo

--- src/js/components/AdobeAppInfo.tsx
+++ src/js/components/AdobeAppInfo.tsx
@@ -12,8 +12,19 @@ const debugPorts = JSON.parse(process.env.DEBUG_PORTS)
 const debugPort = debugPorts[getApplicationID()]
 
 export default class AdobeAppInfo extends React.Component {
-  async render() {
+  public state = {
+    info: null,
+    extensionPath: null
+  }
+
+  async componentDidMount() {
     const info = await evalExtendscript(`$.global["${id}"].getInfo()`)
+    const extensionPath = await getExtensionPath()
+    this.setState({ info, extensionPath })
+  }
+
+  render() {
+    const { info, extensionPath } = this.state
     return (
       <div className="AdobeAppInfo">
         <h3>Adobe App Info</h3>
@@ -21,7 +32,7 @@ export default class AdobeAppInfo extends React.Component {
           <li>Id: {info.id}</li>
           <li>Name: {info.name}</li>
           <li>Version: {info.version}</li>
-          <li>Extension Path: {await getExtensionPath()}</li>
+          <li>Extension Path: {extensionPath}</li>
         </ul>
         <button onClick={() => openURLInDefaultBrowser(`http://localhost:${debugPort}`)}>Open debugger</button>
       </div>
