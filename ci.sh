f0fca8d19799e766921143e48dfd679ddf2155de
Koen Schmeets <hello@koenschmeets.nl>
2019-09-26 17:40:00 +0200

    Drone config

--- ci.sh
+++ ci.sh
@@ -1,4 +1,9 @@
-#!/bin/zsh
+#!/bin/bash
+
+export $HOME=/Users/koen
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
 
 nvm use 10
 npm install
