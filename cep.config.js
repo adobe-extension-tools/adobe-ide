44ebb6e27821a86d81ef8093540d9ed402568999
Koen Schmeets <hello@koenschmeets.nl>
2019-09-26 17:26:20 +0200

    Adding drone

--- cep.config.js
+++ cep.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const { createConfig } = require('cep-bundler-webpack')
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+// const WrapperPlugin = require('wrapper-webpack-plugin')
 
 const config = createConfig({
     devHost: '0.0.0.0', 
@@ -13,4 +14,8 @@ config.plugins.push(new MonacoWebpackPlugin({
     languages: ['json', 'javascript', 'typescript']
 }))
 
+config.mode = process.env.IS_DEV ? 'development' : 'production'
+// config.plugins = config.plugins.filter(p => !(p instanceof WrapperPlugin))
+
+console.log(config)
 module.exports = config
--- extendscript.config.js
+++ extendscript.config.js
@@ -1,8 +1,12 @@
 const path = require('path')
 const { createConfig } = require('cep-bundler-webpack')
 
-module.exports = createConfig({
+const config = createConfig({
     out: path.join(__dirname, 'dist'),
     type: 'extendscript',
     entry: './src/jsx/index.ts'
 });
+
+config.mode = process.env.IS_DEV ? 'development' : 'production'
+
+module.exports = config
