44554c1ca89566e3102433dfbbcb669595366d24
Koen Schmeets <hello@koenschmeets.nl>
2019-09-27 01:56:04 +0200

    Drone config

--- ci.sh
+++ ci.sh
@@ -12,12 +12,12 @@ npm run installer
 
 eval $(docker-machine env default)
 docker run --rm \
-  -e DRONE_BUILD_EVENT=tag \
+  # -e DRONE_BUILD_EVENT=tag \
   -e DRONE_REPO_OWNER=adobe-extension-tools \
   -e DRONE_REPO_NAME=adobe-ide \
   -e DRONE_COMMIT_REF=refs/heads/master \
   -e PLUGIN_API_KEY=${GITHUB_API_KEY} \
   -e PLUGIN_FILES=master \
-  -v ./archive:/data \
-  -w /data \
+  -v archive:/data \
+  -w `pwd` \
   plugins/github-release
