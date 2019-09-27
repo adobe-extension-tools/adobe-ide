07222d0f486c5ad76ae379e6e88317083a1bc8b9
Koen Schmeets <hello@koenschmeets.nl>
2019-09-27 14:28:52 +0200

    Update release script

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export HOME="/Users/koen"
+export PATH=$HOME/bin:/usr/local/bin:/usr/bin:$PATH
+echo $PATH
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+
+nvm use 10
+
+export GH_RELEASE_GITHUB_API_TOKEN=$GITHUB_API_KEY
+npm gh-release --tag_name $DRONE_TAG --work-path ./archive --owner adobe-extension-tools --repo adobe-ide --yes 
