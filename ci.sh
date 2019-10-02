30912ba84c85cf321bc8db3f371bfca078882864
Koen Schmeets <hello@koenschmeets.nl>
2019-10-02 18:54:35 +0200

    Updates

--- ci.sh
+++ ci.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# export HOME="/Users/koen"
 export PATH=$HOME/bin:/usr/local/bin:/usr/bin:$PATH
 export NVM_DIR="$HOME/.nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
